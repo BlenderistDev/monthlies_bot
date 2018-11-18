ea6aed9ffbd72c5acee98d6a0de2db46078098e4
Dmitry Filippov <demonx.blenderist@gmail.com>
2018-11-18 20:05:20 +0300

    Fully working monthlies bot with classes

--- monthlies_bot.js
+++ monthlies_bot.js
@@ -6,6 +6,204 @@ const telegram = new Telegram(TELEGRAM_API_TOKEN);
 const bot = new Telegraf(TELEGRAM_API_TOKEN, {
 	polling: true
 })
+
+class UserObject{
+	constructor(){
+		this.id = undefined;
+		this.time = new Date().getHours();
+		this.lastmsg = null;
+		this.lastcommand = null;
+		this.mes = [];
+	}
+	savetoFile(){
+    this.writeJsonFile();
+  }
+  writeJsonFile(){
+    console.log("write file "+this.id)
+    console.log(JSON.stringify(this).toString())
+    fs.unlink("data/"+this.id+".json",(err)=>{
+      if (err)
+        console.log(err);
+      fs.writeFile("data/"+this.id+".json",JSON.stringify(this),'utf8', function(){});
+    })
+	}
+	timeAfterLastMes(){
+		return (new Date() -  new Date(this.mes[this.mes.length-1].start))
+	}
+	timeAfterLastMsg(){
+		return (new Date() -  new Date(this.lastmsg))
+	}
+	checkIfNotifiationNeed(){
+		var betweenMsg = 1000*60//1000*60*60*24;
+		var betweenMes = 1000*60*3 //1000*60*60*24*28;
+		var betweenStartEnd = 1000*60*2//1000*60*60*24*3;
+		var betweenStartPain =1000*60*1//1000*60*60*24*1;
+		if ( ( (this.mes.length == 0) || (this.timeAfterLastMes() >= betweenMes) ) && ( (this.timeAfterLastMsg() >= (betweenMsg) ) || (this.lastmsg == null) ) && (this.time == (new Date()).getHours()) ){
+			console.log((new Date).getHours(), this.time);
+			console.log("start question user "+this.id)
+			telegram.sendMessage(this.id, 'Месячные начались?', optionsStart);
+			this.lastmsg = new Date();
+			this.savetoFile();
+		}	
+		if (this.mes.length!=0 && this.mes[this.mes.length-1].end==null && (this.timeAfterLastMes() >= betweenStartEnd) && (this.timeAfterLastMsg()>=betweenMsg) && (this.time == (new Date()).getHours())){
+			console.log("end question user "+this.id)
+			telegram.sendMessage(this.id, 'Месячные закончились?', optionsEnd);
+			this.lastmsg = new Date();
+			this.savetoFile();
+		}	
+		if (this.mes.length!=0 &&  (this.timeAfterLastMes() >= betweenStartPain) && (this.timeAfterLastMes() <= betweenStartEnd) && (this.timeAfterLastMsg()>=betweenMsg) && (this.time == (new Date()).getHours())){
+			console.log("first pain question user "+this.id)
+			telegram.sendMessage(this.id, 'Оцени уровень боли', optionsFirstPain);
+			this.lastmsg = new Date();
+			this.savetoFile();
+		}
+	}
+	static readJsonFile(file){
+    return new Promise(function(success,fail){
+      var path = "data/"+file+".json";
+      fs.readFile(path,function(error,data){
+        var obj;
+        if (error){
+          console.log("error with reading file:"+file);
+          console.log(error);
+          obj = new UserObject;
+					obj.id=file;
+					obj.savetoFile();
+        }
+        else{
+          var obj = JSON.parse(data);
+          obj.__proto__ = new UserObject;
+        }
+        success(obj);
+      })
+    }).catch((error)=>{console.log("error with parsing: "+data);})
+	}
+	static check_notifications(){
+    fs.readdir("data/",function(error,data){
+      if (error)
+        conlose.error(error);
+      data.forEach(function(element){
+      if (element){
+        var filename = element.toString();
+        var filename = filename.slice(0,filename.length-".json".length);
+        UserObject.readJsonFile(filename).then(function(obj){
+          obj.checkIfNotifiationNeed();
+        })
+      }
+      })
+    })
+	}
+	mesEnd(){
+		this.mes[this.mes.length-1].end = new Date();
+		this.savetoFile();
+		telegram.sendMessage(this.id,"Оцени уровень боли:",optionsSecondPain);
+	}
+	static painToRussian(pain){
+		switch (pain){
+			case "low":
+				return("не больно");
+			case "normal":
+				return("обычно");
+			case "high":
+				return("больно");
+			default:
+				return("---");
+		}
+	}
+	firstPain(answer){
+		if (/high/.test(answer)){
+			console.log("first pain is high for user "+this.id);
+			this.mes[this.mes.length-1].firstpain="high";
+		}
+		if (/normal/.test(answer)){
+			console.log("first pain is normal for user "+this.id);
+			this.mes[this.mes.length-1].firstpain="normal";
+		}
+		if (/low/.test(answer)){
+			console.log("first pain is low for user "+this.id);
+			this.mes[this.mes.length-1].firstpain="low";
+		}
+		this.savetoFile();
+
+	}
+	secondPain(answer){
+		if (/high/.test(answer)){
+			console.log("second pain is high for user "+this.id);
+			this.mes[this.mes.length-1].secondpain="high";
+		}
+		if (/normal/.test(answer)){
+			console.log("second pain is normal for user "+this.id);
+			this.mes[this.mes.length-1].secondpain="normal";
+		}
+		if (/low/.test(answer)){
+			console.log("second pain is low for user "+this.id);
+			this.mes[this.mes.length-1].secondpain="low";
+		}				
+		this.savetoFile();
+	}
+	tableCreate(){
+		var str ="";
+		if (this.mes.length==0){
+			console.log("history not found for user "+this.id)
+			telegram.sendMessage(this.id,"история не найдена");
+			return;
+		}
+		this.mes.forEach(function(element){
+			element.__proto__=new Monthlies;
+			str += element.toString();
+		});
+		console.log("sending history for user "+this.id);
+		telegram.sendMessage(this.id,str);
+	}
+	static checkTimeCorrect(msg){
+    var hour = msg.message.text;
+		if (/\D+/.test(hour)){
+			telegram.sendMessage(msg.from.id,"Это не цифра");
+			return false;
+		}
+		if(hour>23){
+			telegram.sendMessage(msg.from.id,"В сутках всего 24 часа!")
+			return false;
+		}
+		if(hour<0){
+			telegram.sendMessage(msg.from.id,"некорректное время!")
+			return false;
+		}
+		return true;
+	}
+	addTime(time){
+		this.lastcommand=null;
+		this.time = time;
+		this.savetoFile();
+		telegram.sendMessage(this.id,"Время сообщений  изменено на "+time)
+	}
+ 
+}
+
+class Monthlies{
+	constructor(){
+		this.start = new Date();
+		this.end = null;
+		this.firstpain = null;
+		this.secondpain = null;
+	}
+	addMonthlies(id){
+		var monthlies = this;
+		UserObject.readJsonFile(id).then(function(obj){
+			obj.mes.push(monthlies);
+			obj.savetoFile();
+		});
+	}
+	toString(){
+		var str = "";
+		if (this.end == null)
+			str += new Date(this.start).getDate() +"."+ (new Date(this.start).getMonth()+1) +"."+ new Date(this.start).getFullYear() +" - null\nБоль в начале: " +UserObject.painToRussian(this.firstpain)+"\nБоль в конце: "+UserObject.painToRussian(this.secondpain);
+		else
+			str += new Date(this.start).getDate() +"."+ (new Date(this.start).getMonth()+1) +"."+ new Date(this.start).getFullYear() +" - "+new Date(this.end).getDate() +"."+ (new Date(this.end).getMonth()+1) +"."+ new Date(this.end).getFullYear() +"\nБоль в начале: "+UserObject.painToRussian(this.firstpain)+"\nБоль в конце: "+UserObject.painToRussian(this.secondpain);
+		str +="\n\n"
+		return str;
+	}
+}
 botStart = function(msg){
   var id = msg.from.id;
 	console.log("user "+id+" starts bot")
@@ -23,20 +221,20 @@ botStart = function(msg){
 }
 bot.start(botStart);
 bot.command("history",function(msg){
-	tableCreate(msg.from.id)
+	UserObject.readJsonFile(msg.from.id).then((obj)=>{obj.tableCreate()})
 });
 
 bot.command("time",function(msg){
-	readJsonFile("data/"+msg.from.id+".json").then(function(data){
+	UserObject.readJsonFile(msg.from.id).then(function(data){
 		var id = data.id;
 		data.lastcommand = "time";
+		data.savetoFile();
 		telegram.sendMessage(data.id,"В котором часу тебе будет удобно получать сообщения? Отправь цифру, например 18.");
-		fs.writeFile("data/"+data.id+".json", JSON.stringify(data), 'utf8', function(){});
 		setTimeout(function(){
-      readJsonFile("data/"+id+".json").then(function(data){
+      UserObject.readJsonFile(id).then(function(data){
 				if (data.lastcommand=="time"){
 					data.lastcommand = "null";
-					fs.writeFile("data/"+data.id+".json", JSON.stringify(data), 'utf8', function(){});
+					data.savetoFile();
 					telegram.sendMessage(data.id,"Ты так и не прислала время. Если решишь изменить время сообщений, воспользуйся командой.");
 				}
 			})
@@ -48,80 +246,7 @@ bot.command("time",function(msg){
 
 bot.startPolling();
 
-var readJsonFile = function(file){
-  return new Promise(function(success,fail){
-    fs.readFile(file,function(error,data){
-	  if (error) 
-	    fail(error);
-	  else
-	    success(JSON.parse(data));
-	})
-  })
-} 
-function CreateMonhlies(){
-  this.start = new Date();
-  this.end = null;
-	this.firstpain = null;
-	this.secondpain = null;
-	return this;
-}
-function addMonhlies(id){
-	console.log("addMonhlies")
-	readJsonFile("data/"+id+".json").then(function(data){
-		data.mes.push(new CreateMonhlies());
-		console.log(data.id);
-		fs.writeFile("data/"+data.id+".json", JSON.stringify(data), 'utf8', function(){});
-	});
-}
-
-function timeAfterLastMes(obj){
-  return (new Date() -  new Date(obj.mes[obj.mes.length-1].start))
-}
-function timeAfterLastMsg(obj){
-  return (new Date() -  new Date(obj.lastmsg))
-}
-
-
-function checkIfNotifiationNeed(obj){
-	var now = new Date();
-
-	var betweenMsg = 1000*60*60*24;
-	var betweenMes = 1000*60*60*24*28;
-	var betweenStartEnd = 1000*60*60*24*3;
-	var betweenStartPain =1000*60*60*24*1;
-  if ( ( (obj.mes.length == 0) || (timeAfterLastMes(obj) >= betweenMes) ) && ( (timeAfterLastMsg(obj) >= (betweenMsg) ) || (obj.lastmsg == null) ) && (obj.time == (new Date()).getHours()) ){
-		console.log((new Date).getHours(), obj.time);
-		console.log("start question user "+obj.id)
-		telegram.sendMessage(obj.id, 'Месячные начались?', optionsStart);
-	  obj.lastmsg = new Date();
-	  fs.writeFile("data/"+obj.id+".json", JSON.stringify(obj), 'utf8', function(){});
-	}
-	
-  if (obj.mes.length!=0 && obj.mes[obj.mes.length-1].end==null && (timeAfterLastMes(obj) >= betweenStartEnd) && (timeAfterLastMsg(obj)>=betweenMsg) && (obj.time == (new Date()).getHours())){
-		console.log("end question user "+obj.id)
-		telegram.sendMessage(obj.id, 'Месячные закончились?', optionsEnd);
-		obj.lastmsg = new Date();
-		fs.writeFile("data/"+obj.id+".json", JSON.stringify(obj), 'utf8', function(){});
-	}
-
-	if (obj.mes.length!=0 &&  (timeAfterLastMes(obj) >= betweenStartPain) && (timeAfterLastMes(obj) <= betweenStartEnd) && (timeAfterLastMsg(obj)>=betweenMsg) && (obj.time == (new Date()).getHours())){
-		console.log("first pain question user "+obj.id)
-		telegram.sendMessage(obj.id, 'Оцени уровень боли', optionsFirstPain);
-		obj.lastmsg = new Date();
-		fs.writeFile("data/"+obj.id+".json", JSON.stringify(obj), 'utf8', function(){});
-	}
-}
-
-function check_notifications(){
-  fs.readdir("data/",function(error,data){
-    data.forEach(function(element){
-	  if (element)
-	    readJsonFile("data/"+element).then(checkIfNotifiationNeed);
-	})
-  });
-}
-
-setInterval(check_notifications,5000);
+setInterval(UserObject.check_notifications,5000);
 
 var optionsStart = {
 	reply_markup: JSON.stringify({
@@ -161,133 +286,37 @@ var optionsSecondPain = {
 };
 
 bot.on('text',function(msg){
-	readJsonFile("data/"+msg.chat.id+".json").then(function(obj){
-		if (obj.lastcommand == "time")
-		  parseTime(msg);
+	UserObject.readJsonFile(msg.chat.id).then(function(obj){
+		if (obj.lastcommand == "time" && UserObject.checkTimeCorrect(msg))
+		  obj.addTime(msg.message.text);
 	})
 });
-bot.on('callback_query', function (msg) {
+bot.on('callback_query', function(msg){
 	var answer = msg.callbackQuery.data;
 	console.log(answer)
 	if (answer == "mes_start"){
-		addMonhlies(msg.from.id);
+		var monthlies = new Monthlies;
+		monthlies.addMonthlies(msg.from.id);
 		telegram.sendMessage(msg.from.id,"Записал!");
 		return;
 	}
 	if (answer == "mes_end"){
-		mesEnd(msg.from.id);
-		telegram.sendMessage(msg.from.id,"Записал!");
-		return;
+    UserObject.readJsonFile(msg.from.id).then(function(obj){
+			telegram.sendMessage(msg.from.id,"Записал!");
+			obj.mesEnd();
+		})
 	}
 	if (/first_pain/.test(answer)){
-		firstpain(msg.from.id,answer);
-		telegram.sendMessage(msg.from.id,"Записал!");
-		return;
+		UserObject.readJsonFile(msg.from.id).then(function(obj){
+			telegram.sendMessage(msg.from.id,"Записал!");
+			obj.firstPain(answer);
+		})
+
 	}
 	if (/second_pain/.test(answer)){
-		secondpain(msg.from.id,answer);
-		telegram.sendMessage(msg.from.id,"Записал!");
-		return;
-	}
+		UserObject.readJsonFile(msg.from.id).then(function(obj){
+			telegram.sendMessage(msg.from.id,"Записал!");
+			obj.secondPain(answer);
+		})
+ }
 });
-
-function mesEnd(id){
-	readJsonFile("data/"+id+".json").then(function(data){
-		data.mes[data.mes.length-1].end = new Date();
-		fs.writeFile("data/"+data.id+".json", JSON.stringify(data), 'utf8', function(){});
-		telegram.sendMessage(data.id,"Оцени уровень боли:",optionsSecondPain);
-	})
-}
-
-function firstpain(id,answer){
-	readJsonFile("data/"+id+".json").then(function(data){
-		if (/high/.test(answer)){
-			console.log("first pain is high for user "+id);
-			data.mes[data.mes.length-1].firstpain="high";
-		}
-		if (/normal/.test(answer)){
-			console.log("first pain is normal for user "+id);
-			data.mes[data.mes.length-1].firstpain="normal";
-		}
-		if (/low/.test(answer)){
-			console.log("first pain is low for user "+id);
-			data.mes[data.mes.length-1].firstpain="low";
-		}
-		fs.writeFile("data/"+data.id+".json", JSON.stringify(data), 'utf8', function(){});
-	})
-}
-function secondpain(id,answer){
-	readJsonFile("data/"+id+".json").then(function(data){
-		if (/high/.test(answer)){
-			console.log("second pain is high for user "+id);
-			data.mes[data.mes.length-1].secondpain="high";
-		}
-		if (/normal/.test(answer)){
-			console.log("second pain is normal for user "+id);
-			data.mes[data.mes.length-1].secondpain="normal";
-		}
-		if (/low/.test(answer)){
-			console.log("second pain is low for user "+id);
-			data.mes[data.mes.length-1].secondpain="low";
-		}				
-		fs.writeFile("data/"+data.id+".json", JSON.stringify(data), 'utf8', function(){});
-	})
-}
-function tableCreate(id){
-	readJsonFile("data/"+id+".json").then(function(data){
-		var str ="";
-		if (data.mes.length==0){
-			console.log("history not found for user "+id)
-			telegram.sendMessage(id,"история не найдена");
-      return;
-		}
-		data.mes.forEach(function(element){
-      str += mesToString(element);
-		});
-		console.log("sending history for user "+id);
-		telegram.sendMessage(id,str);
-	});
-}
-function mesToString(mes) {
-  var str = "";
-	if (mes.end == null)
-	  str += new Date(mes.start).getDate() +"."+ (new Date(mes.start).getMonth()+1) +"."+ new Date(mes.start).getFullYear() +" - null\nБоль в начале: " +painToRussian(mes.firstpain)+"\nБоль в конце: "+painToRussian(mes.secondpain);
-	else
-		str += new Date(mes.start).getDate() +"."+ (new Date(mes.start).getMonth()+1) +"."+ new Date(mes.start).getFullYear() +" - "+new Date(mes.end).getDate() +"."+ (new Date(mes.end).getMonth()+1) +"."+ new Date(mes.end).getFullYear() +"\nБоль в начале: "+painToRussian(mes.firstpain)+"\nБоль в конце: "+painToRussian(mes.secondpain);
-	str +="\n\n"
-	return str;
-}
-function painToRussian(pain){
-	switch (pain){
-		case "low":
-			return("не больно");
-		case "normal":
-			return("обычно");
-		case "high":
-			return("больно");
-		default:
-		  return("---");
-	}
-}
-function parseTime(msg){
-	var hour = msg.message.text;
-	if (/\D+/.test(hour)){
-		telegram.sendMessage(msg.from.id,"Это не цифра");
-		return;
-	}
-	if(hour>23){
-		telegram.sendMessage(msg.from.id,"В сутках всего 24 часа!")
-		return;
-	}
-	if(hour<0){
-		telegram.sendMessage(msg.from.id,"некорректное время!")
-		return;
-	}
-	readJsonFile("data/"+msg.from.id+".json").then(function(data){
-		data.lastcommand=null;
-		data.time = hour;
-		fs.writeFile("data/"+data.id+".json", JSON.stringify(data), 'utf8', function(){});
-		telegram.sendMessage(msg.from.id,"Время сообщений  изменено на "+hour)
-	})
-
-}
